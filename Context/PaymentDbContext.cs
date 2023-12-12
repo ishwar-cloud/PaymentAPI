10c08af905cdddef2466c9482cc5cdafe8e59a78
ishwar-cloud <ishwarm2017@gmail.com>
2023-12-12 22:33:28 +0530

    initial commit

---
+++ Context/PaymentDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PaymentAPI.Models;
+
+namespace PaymentAPI.Context
+{
+    public class PaymentDbContext : DbContext
+    {
+        //constractor
+
+        public PaymentDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        // creating table in database
+        public DbSet<PaymentDetails> PaymentDetails { get; set; }
+    }
+
+}
