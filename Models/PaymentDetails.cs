1447f0e0c31632df8974e6d9902cc4098ea9290a
ishwar-cloud <ishwarm2017@gmail.com>
2023-12-13 12:20:06 +0530

    Update the migration and adding validation to cardOwnername and cardNumber

--- Models/PaymentDetails.cs
+++ Models/PaymentDetails.cs
@@ -10,15 +10,17 @@ namespace PaymentAPI.Models
 
         [Required]
         [Column(TypeName ="nvarchar(100)")]
+        [RegularExpression(@"^[a-zA-Z\s]*$", ErrorMessage = "Only alphabets are allowed for Card Owner Name")]
         public string? CardOwnerName { get; set; }
         [Required]
         [Column(TypeName = "nvarchar(16)")]
-
+        [RegularExpression(@"^[0-9]*$", ErrorMessage = "Only numbers are allowed for Card Number")]
         public string? CardNumber { get; set; }
         [Required]
         [Column(TypeName = "nvarchar(5)")]
 
         public string? ExpirationDate { get; set; }
+
         [Required]
         [Column(TypeName = "nvarchar(3)")]
 
